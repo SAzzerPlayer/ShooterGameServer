b617934d9290addca27a058ba5e348ca88942994
Lohvyn Danyil <logvin24820@gmail.com>
2020-05-12 13:21:47 +0300

    Fixed port of the server

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ import express = require('express');
 import upd from './path/path';
 // Create a new express app instance
 const app: express.Application = express();
-console.log(upd);
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-app.listen(3000, function () {
-    console.log('App is listening on port 3000!');
-});
+app.listen(port, () => {console.log(upd)});
+console.log('Server running on http://%s:%s', ip, port);
